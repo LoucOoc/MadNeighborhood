ddd57b05b4b73f8bb349f3ed40a7e16a5de36976
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2023-11-11 14:54:21 -0600

    first commit

---
+++ src/main/java/tech/madneighborhood/MadNeighborhoodApplication.java
@@ -0,0 +1,13 @@
+package tech.madneighborhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadNeighborhoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadNeighborhoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/madneighborhood/MadNeighborhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.madneighborhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadNeighborhoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
