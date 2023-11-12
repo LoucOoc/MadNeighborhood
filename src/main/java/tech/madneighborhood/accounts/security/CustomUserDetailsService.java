7c90b82d5b1f9888ba59aaa78c847cb1264355fe
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2023-11-11 19:06:20 -0600

    updates

--- src/main/java/tech/madneighborhood/accounts/security/CustomUserDetailsService.java
+++ src/main/java/tech/madneighborhood/accounts/security/CustomUserDetailsService.java
@@ -6,12 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import tech.madneighborhood.accounts.entity.Role;
 import tech.madneighborhood.accounts.entity.User;
 import tech.madneighborhood.accounts.repository.UserRepository;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.LinkedList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -29,15 +28,21 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (user != null) {
             return new org.springframework.security.core.userdetails.User(user.getEmail(),
                     user.getPassword(),
-                    mapRolesToAuthorities(user.getRoles()));
+                    mapRolesToAuthorities());
         } else {
             throw new UsernameNotFoundException("Invalid username or password.");
         }
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities() {
+        return new LinkedList<>(){{
+            add(new SimpleGrantedAuthority(""));
+        }};
+        /*
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
+
+         */
     }
 }
--- src/main/java/tech/madneighborhood/accounts/service/UserServiceImpl.java
+++ src/main/java/tech/madneighborhood/accounts/service/UserServiceImpl.java
@@ -3,12 +3,9 @@ package tech.madneighborhood.accounts.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import tech.madneighborhood.accounts.dto.UserDto;
-import tech.madneighborhood.accounts.entity.Role;
 import tech.madneighborhood.accounts.entity.User;
-import tech.madneighborhood.accounts.repository.RoleRepository;
 import tech.madneighborhood.accounts.repository.UserRepository;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,13 +13,11 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService{
 
     private UserRepository userRepository;
-    private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         super();
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -34,11 +29,6 @@ public class UserServiceImpl implements UserService{
 
         user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
-        Role role = roleRepository.findByName(Role.ROLE_USER);
-        if(role == null) {
-            role = checkRoleExist(Role.ROLE_USER);
-        }
-        user.setRoles(Arrays.asList(role));
         userRepository.save(user);
     }
 
@@ -62,9 +52,4 @@ public class UserServiceImpl implements UserService{
         return userDto;
     }
 
-    private Role checkRoleExist(String roleName){
-        Role role = new Role();
-        role.setName(roleName);
-        return roleRepository.save(role);
-    }
 }
